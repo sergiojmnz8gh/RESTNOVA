abe5b528e27f45766c40b01345a2cd9cb9f8604c
sergiojmnz8gh <jimenezelichesergio@gmail.com>
2026-03-29 23:44:20 +0200

    Initial commit

---
+++ BACKEND/restnova/src/main/java/com/sje/restnova/RestnovaApplication.java
@@ -0,0 +1,13 @@
+package com.sje.restnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestnovaApplication.class, args);
+	}
+
+}
---
+++ BACKEND/restnova/src/test/java/com/sje/restnova/RestnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sje.restnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
